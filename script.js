e2cbbeb943853ac704d077af05361d10598decb8
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-10-17 11:01:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
-  let ans = num.toString(2);
-	console.log(ans);
+ if(num === 0)
+ {
+	 return '0';
+ }
+	let binary = "";
+
+	while(num > 0)
+		{
+			let rem = num%2;
+			binary = rem + binary;
+			num = Math.floor(num/2);
+		}
+	console.log(binary);
 }
 
 window.decimalToBinary = decimalToBinary;
